819738e67acd2352cf13834bba93298928f8abbe
Emiliano Torres <ehwachintopiola@gmail.com>
2022-11-25 23:04:00 -0300

    arreglo-reentrega

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import ItemListContainer from './ItemListContainer/ItemListContainer';
 import Navbar from './Navbar/Navbar';
+
 const App = () => {
   return (
     <>
       <Navbar />
+      <ItemListContainer greeting={"Hola Mundo"}/>
     </>
   );
 }
